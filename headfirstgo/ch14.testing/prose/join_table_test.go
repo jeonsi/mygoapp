46ff2c57afd60c36516e1fc2f3c7a700b9283a07
Seung-il Jeon <si.j@navercorp.com>
2022-01-04 23:56:51 +0900

    refactoring

--- headfirstgo/ch14.testing/prose/join_table_test.go
+++ headfirstgo/ch14.testing/prose/join_table_test.go
@@ -9,28 +9,16 @@ type testData struct {
 
 func TestJoinWithCommas(t *testing.T) {
 	tests := []testData{
-		{
-			list: []string{},
-			want: "",
-		},
-		{
-			list: []string{"apple"},
-			want: "apple",
-		},
-		{
-			list: []string{"apple", "orange"},
-			want: "apple and orange",
-		},
-		{
-			list: []string{"apple", "orange", "pear"},
-			want: "apple, orange, and pear",
-		},
+		{list: []string{}, want: ""},
+		{list: []string{"apple"}, want: "apple"},
+		{list: []string{"apple", "orange"}, want: "apple and orange"},
+		{list: []string{"apple", "orange", "pear"}, want: "apple, orange, and pear"},
 	}
 
 	for _, test := range tests {
 		got := JoinWithCommas(test.list)
 		if got != test.want {
-			t.Errorf(errorString(test.list, got, test.want))
+			t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", test.list, got, test.want)
 		}
 	}
 }
